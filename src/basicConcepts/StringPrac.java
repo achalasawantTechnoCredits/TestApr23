244820325cb77037e654adb7c24e64d938621f00
Achala Sawant <achala.sawant@gmail.com>
2023-05-23 08:13:01 +0530

    Done changes in SpringPrac

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -3,7 +3,8 @@ package basicConcepts;
 public class StringPrac {
 
 	public static void main(String[] args) {
-		System.out.println("I am in main method");
+		System.out.println("I am in main method.of Branch A");
+		System.out.println("Stash msg");
 	}
 
 }
